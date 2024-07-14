0d253aedaaab67c730da4862f475145347239e65
ck-linuxx <amorimclark@gmail.com>
2024-07-14 01:13:25 -0300

    featch: Implementing db connection with repository

--- init/schema.sql
+++ init/schema.sql
@@ -19,7 +19,5 @@ CREATE TABLE IF NOT EXISTS 'links' (
     id TEXT PRIMARY KEY,
     trip_id TEXT,
     link TEXT NOT NULL,
-    title TEXT NOT NULL,
     FOREIGN KEY (trip_id) REFERENCES trips(id)
 );
-
