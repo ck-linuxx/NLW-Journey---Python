c1d69dcbff70c11c02e65a4892d40763824838f3
ck-linuxx <amorimclark@gmail.com>
2024-07-15 08:54:48 -0300

    featch: Implementing activities and participants repository

---
+++ src/models/repositories/activities_repository.py
@@ -0,0 +1,31 @@
+from typing import Dict, Tuple, List
+from sqlite3 import Connection
+
+class ActivitiesRepository:
+  def __init__(self, conn: Connection) -> None:
+    self.__conn = conn
+
+  def registry_activity(self, activities_infos: Dict) -> None:
+    cursor = self.__conn.cursor()
+    cursor.execute(
+      '''
+        INSERT INTO activities
+          (id, trip_id, title, occurs_at)
+        VALUES
+          (? ,? ,? ,?)
+      ''', (
+        activities_infos["id"],
+        activities_infos["trip_id"],
+        activities_infos["title"],
+        activities_infos["occurs_at"]
+      )
+    )
+    self.__conn.commit()
+
+  def find_activities_from_trip(self, trip_id: str) -> List[Tuple]:
+      cursor = self.__conn.cursor()
+      cursor.execute(
+          '''SELECT * FROM activities WHERE trip_id = ?''', (trip_id,)
+      )
+      activities = cursor.fetchall()
+      return activities
